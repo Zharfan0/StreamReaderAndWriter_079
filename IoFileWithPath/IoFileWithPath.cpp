5dc3a25efbd3268216c0266e5cd5edbdbb40c145
Zharfan0 <zharfanwidyadana@gmail.com>
2023-06-21 11:07:40 +0700

    menampilkan dan membaca

--- IoFileWithPath/IoFileWithPath.cpp
+++ IoFileWithPath/IoFileWithPath.cpp
@@ -27,4 +27,22 @@ int main() {
 		//menulis dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl;
 	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	//jika file ada maka
+	if (infile.is_open())
+	{
+		//melakukan perulangan tiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+	}
 }
